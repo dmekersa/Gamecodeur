2ce2f77112191734fd0195e4ad1869e6e8d3ca1d
David Mekersa <dmekersa@gmail.com>
2016-04-28 16:32:58 +0200

    Squelette minimum Love2D / Zerobrane Studio

---
+++ SqueletteMini/main.lua
@@ -0,0 +1,31 @@
+-- Cette ligne permet d'afficher des traces dans la console pendant l'éxécution
+io.stdout:setvbuf('no')
+
+-- Empèche Love de filtrer les contours des images quand elles sont redimentionnées
+-- Indispensable pour du pixel art
+love.graphics.setDefaultFilter("nearest")
+
+-- Cette ligne permet de déboguer pas à pas dans ZeroBraneStudio
+if arg[#arg] == "-debug" then require("mobdebug").start() end
+
+function love.load()
+  
+  largeur = love.graphics.getWidth()
+  hauteur = love.graphics.getHeight()
+  
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+    
+end
+
+function love.keypressed(key)
+  
+  print(key)
+  
+end
+  
