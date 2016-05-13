3cf42548e2ac7e4b7061eaad68ac2f2ea225921b
David Mekersa <dmekersa@gmail.com>
2016-05-13 10:05:47 +0200

    no message

--- AtelierDonjon1-Lua/main.lua
+++ AtelierDonjon1-Lua/main.lua
@@ -1,26 +1,45 @@
 local donjon = {}
-donjon.largeur = 9
-donjon.hauteur = 6
+donjon.nombreDeColonnes = 9
+donjon.nombreDeLignes = 6
 donjon.map = {}
 
 function genereDonjon()
   donjon.map = {}
   
   local nLigne,nColonne
-  for nLigne=1,donjon.hauteur do
-    
-    for nColonne=1,donjon.largeur do
+  for nLigne=1,donjon.nombreDeLignes do
+    donjon.map[nLigne] = {}
+    for nColonne=1,donjon.nombreDeColonnes do
+      donjon.map[nLigne][nColonne] = nil
     end
-    
   end
   
 end
 
 function love.load()
+  genereDonjon()
 end
 
 function love.update(dt)
 end
 
 function love.draw()
+  local x,y
+  x=5
+  y=5
+  local largeurCase = 34
+  local hauteurCase = 13
+  local espaceCase = 5
+  local nLigne,nColonne
+  for nLigne=1,donjon.nombreDeLignes do
+    x = 5
+    for nColonne=1,donjon.nombreDeColonnes do
+      if donjon.map[nLigne][nColonne] == nil then
+        love.graphics.setColor(60,60,60)
+        love.graphics.rectangle("fill",x,y,largeurCase,hauteurCase)
+      end
+      x = x + largeurCase + espaceCase
+    end
+    y = y + hauteurCase + espaceCase
+  end
 end
