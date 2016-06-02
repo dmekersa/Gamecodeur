2fb03587cb08faf2417963e501dd9fb9d3f3b630
David Mekersa <dmekersa@gmail.com>
2016-06-02 16:23:51 +0200

    no message

---
+++ Atelier Redemption of Sam/Step11/bulletmanager.lua
@@ -0,0 +1,74 @@
+local bulletManager = {}
+
+local gameState = require("gamestate")
+local particleManager = require("particlemanager")
+
+bulletManager.liste_bullets = {}
+
+bulletManager.Reset = function()
+  bulletManager.liste_bullets = {}
+end
+
+bulletManager.CreeBullet = function(pType, pX, pY, pVx, pVy, pLife)
+  
+  if pLife == nil then
+    pLife = 50
+  end
+  
+  local bullet = {}
+  bullet.type = pType
+  bullet.x = pX
+  bullet.y = pY
+  bullet.vx = pVx
+  bullet.vy = pVy
+  bullet.life = pLife
+  bullet.supprime = false
+  table.insert(bulletManager.liste_bullets, bullet)
+  
+  bullet.Anime = function(dt)
+  end
+  
+  bullet.Deplace = function(dt)
+    -- Application de la vélocité
+    bullet.x = bullet.x + bullet.vx
+    bullet.y = bullet.y + bullet.vy
+    bullet.life = bullet.life - 1
+    if bullet.life < 0 then
+      bullet.supprime = true
+      particleManager.CreeExplosion("touchesol", bullet.x, bullet.y)
+    end
+  end
+  
+  return bullet
+  
+end
+
+bulletManager.update = function(dt)
+  local n
+  for n=#bulletManager.liste_bullets,1,-1 do
+    local s = bulletManager.liste_bullets[n]
+    
+    local nColonneCollision
+    local nLigneCollision
+    nColonneCollision = math.floor((s.x / LARGEURTILE) + 1)
+    nLigneCollision = math.floor((s.y / HAUTEURTILE) + 1)
+    
+    if gameState.mapMurs[nLigneCollision][nColonneCollision] > 0 or s.supprime == true then
+      table.remove(bulletManager.liste_bullets,n)
+    end
+    
+    s.Deplace(dt)
+    s.Anime(dt)
+  end
+end
+
+bulletManager.draw = function()
+  local n
+  for n=1,#bulletManager.liste_bullets do
+    local s = bulletManager.liste_bullets[n]
+    --love.graphics.draw(frame, s.x, s.y, 0, s.flip, 1, s.l/2, s.h/2)
+    love.graphics.circle("fill",s.x,s.y,10)
+  end
+end
+
+return bulletManager
---
+++ Atelier Redemption of Sam/Step11/particlemanager.lua
@@ -0,0 +1,83 @@
+local particleManager = {}
+
+local gameState = require("gamestate")
+
+particleManager.liste_particles = {}
+
+particleManager.Reset = function()
+  particleManager.liste_particles = {}
+end
+
+particleManager.CreeExplosion = function(pType, pX, pY)
+  
+  if pType == "touchesol" then
+    local n
+    for n=1,5 do
+      particleManager.CreeParticle(pType, pX, pY, math.random(-30,30)/10,math.random(-10,-40)/10,math.random(5,15))
+    end
+  end
+
+end
+
+particleManager.CreeParticle = function(pType, pX, pY, pVx, pVy, pLife)
+  
+  if pLife == nil then
+    pLife = 50
+  end
+  
+  local particle = {}
+  particle.type = pType
+  particle.x = pX
+  particle.y = pY
+  particle.vx = pVx
+  particle.vy = pVy
+  particle.life = pLife
+  particle.supprime = false
+  table.insert(particleManager.liste_particles, particle)
+  
+  particle.Anime = function(dt)
+  end
+  
+  particle.Deplace = function(dt)
+    -- Application de la vélocité
+    particle.x = particle.x + particle.vx
+    particle.y = particle.y + particle.vy
+    particle.life = particle.life - 1
+    if particle.life < 0 then
+      particle.supprime = true
+    end
+  end
+  
+  return particle
+  
+end
+
+particleManager.update = function(dt)
+  local n
+  for n=#particleManager.liste_particles,1,-1 do
+    local s = particleManager.liste_particles[n]
+    
+    local nColonneCollision
+    local nLigneCollision
+    nColonneCollision = math.floor((s.x / LARGEURTILE) + 1)
+    nLigneCollision = math.floor((s.y / HAUTEURTILE) + 1)
+    
+    if gameState.mapMurs[nLigneCollision][nColonneCollision] > 0 or s.supprime == true then
+      table.remove(particleManager.liste_particles,n)
+    end
+    
+    s.Deplace(dt)
+    s.Anime(dt)
+  end
+end
+
+particleManager.draw = function()
+  local n
+  for n=1,#particleManager.liste_particles do
+    local s = particleManager.liste_particles[n]
+    --love.graphics.draw(frame, s.x, s.y, 0, s.flip, 1, s.l/2, s.h/2)
+    love.graphics.circle("fill",s.x,s.y,2)
+  end
+end
+
+return particleManager
